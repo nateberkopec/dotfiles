7414cbfbac88a6274667c48fd06362e8f12f8cc7
Nate Berkopec <nate.berkopec@gmail.com>
2026-01-26 07:18:48 +0900

    Add Spotlight battery indexing toggle

---
+++ lib/dotfiles/steps/configure_spotlight_indexing_step.rb
@@ -0,0 +1,214 @@
+require "shellwords"
+
+class Dotfiles::Step::ConfigureSpotlightIndexingStep < Dotfiles::Step
+  macos_only
+
+  def self.depends_on
+    [Dotfiles::Step::InstallBrewPackagesStep]
+  end
+
+  def run
+    install_battery_toggle if battery_mode_enabled?
+    disable_configured_volumes if disabled_volumes.any?
+  end
+
+  def complete?
+    super
+    return true unless battery_mode_enabled? || disabled_volumes.any?
+
+    if battery_mode_enabled?
+      add_error("Fish not found for Spotlight battery toggle") unless fish_path
+      add_error("Spotlight battery script not installed at #{script_path}") unless script_installed?
+      add_error("LaunchDaemon not installed at #{launchdaemon_path}") unless launchdaemon_installed?
+    end
+
+    disabled_volumes.each do |volume|
+      add_error("Spotlight indexing still enabled for #{volume}") unless indexing_disabled?(volume)
+    end
+
+    @errors.empty?
+  end
+
+  private
+
+  def install_battery_toggle
+    return unless fish_path
+
+    install_script unless script_installed?
+    install_launchdaemon unless launchdaemon_installed?
+    load_launchdaemon
+  end
+
+  def install_script
+    debug "Installing Spotlight battery script to #{script_path}..."
+    @system.mkdir_p(script_dir)
+    @system.write_file(script_path, script_content)
+    @system.chmod(0o755, script_path)
+  end
+
+  def install_launchdaemon
+    debug "Installing LaunchDaemon to #{launchdaemon_path}..."
+    @system.mkdir_p(script_dir)
+    @system.write_file(launchdaemon_source_path, plist_content)
+    execute("install -m 644 #{shell_escape(launchdaemon_source_path)} #{shell_escape(launchdaemon_path)}", sudo: true)
+  end
+
+  def load_launchdaemon
+    debug "Loading LaunchDaemon..."
+    execute("launchctl bootout system #{shell_escape(launchdaemon_path)} 2>/dev/null || true", sudo: true)
+    execute("launchctl bootstrap system #{shell_escape(launchdaemon_path)}", sudo: true)
+  end
+
+  def disable_configured_volumes
+    disabled_volumes.each do |volume|
+      next if indexing_disabled?(volume)
+
+      execute("mdutil -i off #{shell_escape(volume)}", sudo: true)
+    end
+  end
+
+  def indexing_disabled?(volume)
+    output, status = execute("mdutil -s #{shell_escape(volume)}", quiet: true)
+    return false unless status == 0
+    output.downcase.include?("disabled")
+  end
+
+  def script_content
+    <<~FISH
+      #!#{fish_path}
+
+      set -l volumes $argv
+      if test (count $volumes) -eq 0
+        exit 0
+      end
+
+      set -l power_line (/usr/bin/pmset -g batt | head -n 1)
+      if string match -q "*Battery Power*" -- $power_line
+        set -l desired off
+      else if string match -q "*AC Power*" -- $power_line
+        set -l desired on
+      else
+        exit 0
+      end
+
+      for volume in $volumes
+        set -l status_output (/usr/bin/mdutil -s $volume 2>/dev/null)
+        set -l status_code $status
+        if test $status_code -ne 0
+          continue
+        end
+
+        set -l status_line (string lower -- (string join " " $status_output))
+        if test $desired = "off"
+          if string match -q "*indexing enabled*" -- $status_line
+            /usr/bin/mdutil -i off $volume
+          end
+        else
+          if string match -q "*disabled*" -- $status_line
+            /usr/bin/mdutil -i on $volume
+          end
+        end
+      end
+    FISH
+  end
+
+  def plist_content
+    <<~PLIST
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+          <key>Label</key>
+          <string>#{launchdaemon_label}</string>
+          <key>ProgramArguments</key>
+          <array>
+      #{plist_program_arguments}
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>StartInterval</key>
+          <integer>#{check_interval_seconds}</integer>
+          <key>StandardOutPath</key>
+          <string>/tmp/spotlight-battery.out</string>
+          <key>StandardErrorPath</key>
+          <string>/tmp/spotlight-battery.err</string>
+      </dict>
+      </plist>
+    PLIST
+  end
+
+  def plist_program_arguments
+    args = [fish_path, script_path, *battery_volumes]
+    args.map { |arg| "        <string>#{arg}</string>" }.join("\n")
+  end
+
+  def fish_path
+    @fish_path ||= begin
+      output, status = @system.execute("command -v fish 2>/dev/null")
+      path = output.strip if status == 0 && !output.strip.empty?
+      path || fallback_fish_path
+    end
+  end
+
+  def fallback_fish_path
+    ["/opt/homebrew/bin/fish", "/usr/local/bin/fish", "/usr/bin/fish"].find { |path| @system.file_exist?(path) }
+  end
+
+  def script_dir
+    File.join(@home, ".local", "share", "spotlight")
+  end
+
+  def script_path
+    File.join(script_dir, "spotlight-battery.fish")
+  end
+
+  def launchdaemon_source_path
+    File.join(script_dir, "com.user.spotlight-battery.plist")
+  end
+
+  def launchdaemon_path
+    "/Library/LaunchDaemons/com.user.spotlight-battery.plist"
+  end
+
+  def launchdaemon_label
+    "com.user.spotlight-battery"
+  end
+
+  def script_installed?
+    @system.file_exist?(script_path)
+  end
+
+  def launchdaemon_installed?
+    @system.file_exist?(launchdaemon_path)
+  end
+
+  def spotlight_settings
+    @spotlight_settings ||= @config.fetch("spotlight_settings", {})
+  end
+
+  def battery_mode_enabled?
+    spotlight_settings.fetch("battery_disable", false)
+  end
+
+  def battery_volumes
+    volumes = spotlight_settings.fetch("battery_volumes", default_battery_volumes)
+    Array(volumes).compact.uniq - disabled_volumes
+  end
+
+  def disabled_volumes
+    Array(spotlight_settings.fetch("disabled_volumes", [])).compact.uniq
+  end
+
+  def check_interval_seconds
+    interval = spotlight_settings.fetch("check_interval_seconds", 60).to_i
+    interval.positive? ? interval : 60
+  end
+
+  def default_battery_volumes
+    ["/", "/System/Volumes/Data"]
+  end
+
+  def shell_escape(value)
+    Shellwords.escape(value)
+  end
+end
---
+++ test/dotfiles/steps/configure_spotlight_indexing_step_test.rb
@@ -0,0 +1,110 @@
+require "test_helper"
+
+class ConfigureSpotlightIndexingStepTest < StepTestCase
+  step_class Dotfiles::Step::ConfigureSpotlightIndexingStep
+
+  def test_should_run_when_battery_mode_enabled_and_missing_launchdaemon
+    write_spotlight_config
+    stub_fish_path
+    stub_macos
+
+    assert_should_run
+  end
+
+  def test_run_installs_script_and_launchdaemon
+    write_spotlight_config
+    stub_fish_path
+
+    step.run
+
+    assert_command_run(:mkdir_p, script_dir)
+    assert_command_run(:chmod, 0o755, script_path)
+    assert @fake_system.file_exist?(script_path)
+    assert @fake_system.file_exist?(launchdaemon_source_path)
+
+    assert_executed("sudo install -m 644 #{launchdaemon_source_path} #{launchdaemon_path}", quiet: false)
+    assert_executed("sudo launchctl bootout system #{launchdaemon_path} 2>/dev/null || true", quiet: false)
+    assert_executed("sudo launchctl bootstrap system #{launchdaemon_path}", quiet: false)
+
+    plist = @fake_system.read_file(launchdaemon_source_path)
+    assert_includes plist, fish_path
+    assert_includes plist, script_path
+    assert_includes plist, "<string>/</string>"
+    assert_includes plist, "<string>/System/Volumes/Data</string>"
+  end
+
+  def test_run_disables_configured_volumes
+    write_spotlight_config("battery_disable" => false, "disabled_volumes" => ["/Volumes/Archive"])
+    stub_mdutil_status("/Volumes/Archive", "Indexing enabled.")
+
+    step.run
+
+    assert_executed("sudo mdutil -i off /Volumes/Archive", quiet: false)
+  end
+
+  def test_complete_when_battery_mode_installed
+    write_spotlight_config
+    stub_fish_path
+    @fake_system.write_file(script_path, "")
+    @fake_system.write_file(launchdaemon_path, "")
+
+    assert_complete
+  end
+
+  def test_incomplete_when_indexing_enabled_on_disabled_volume
+    write_spotlight_config("battery_disable" => false, "disabled_volumes" => ["/Volumes/Archive"])
+    stub_mdutil_status("/Volumes/Archive", "Indexing enabled.")
+
+    assert_incomplete
+  end
+
+  def test_complete_when_indexing_disabled_on_disabled_volume
+    write_spotlight_config("battery_disable" => false, "disabled_volumes" => ["/Volumes/Archive"])
+    stub_mdutil_status("/Volumes/Archive", "Indexing disabled.")
+
+    assert_complete
+  end
+
+  private
+
+  def write_spotlight_config(overrides = {})
+    settings = {
+      "battery_disable" => true,
+      "battery_volumes" => ["/", "/System/Volumes/Data"],
+      "check_interval_seconds" => 60
+    }.merge(overrides)
+    write_config("spotlight", "spotlight_settings" => settings)
+  end
+
+  def stub_fish_path
+    @fake_system.stub_command("command -v fish 2>/dev/null", fish_path, 0)
+  end
+
+  def stub_macos
+    @fake_system.define_singleton_method(:macos?) { true }
+  end
+
+  def stub_mdutil_status(volume, status_line)
+    @fake_system.stub_command("mdutil -s #{volume}", "#{volume}:\n\t#{status_line}", 0)
+  end
+
+  def fish_path
+    "/opt/homebrew/bin/fish"
+  end
+
+  def script_dir
+    File.join(@home, ".local", "share", "spotlight")
+  end
+
+  def script_path
+    File.join(script_dir, "spotlight-battery.fish")
+  end
+
+  def launchdaemon_source_path
+    File.join(script_dir, "com.user.spotlight-battery.plist")
+  end
+
+  def launchdaemon_path
+    "/Library/LaunchDaemons/com.user.spotlight-battery.plist"
+  end
+end
