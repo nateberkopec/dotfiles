c7df54740cbcf5c6c1faee937e936aaaf11cfc96
Nate Berkopec <nate.berkopec@gmail.com>
2026-01-26 07:26:30 +0900

    Exclude non-root paths from Spotlight via metadata file

--- test/dotfiles/steps/configure_spotlight_indexing_step_test.rb
+++ test/dotfiles/steps/configure_spotlight_indexing_step_test.rb
@@ -35,6 +35,7 @@ class ConfigureSpotlightIndexingStepTest < StepTestCase
 
   def test_run_disables_configured_volumes
     write_spotlight_config("battery_disable" => false, "disabled_volumes" => ["/Volumes/Archive"])
+    stub_df_mount("/Volumes/Archive", "/Volumes/Archive")
     stub_mdutil_status("/Volumes/Archive", "Indexing enabled.")
 
     step.run
@@ -44,11 +45,12 @@ class ConfigureSpotlightIndexingStepTest < StepTestCase
 
   def test_run_expands_tilde_for_disabled_volumes
     write_spotlight_config("battery_disable" => false, "disabled_volumes" => ["~/Documents/Code.nosync"])
-    stub_mdutil_status(expanded_home_code_dir, "Indexing enabled.")
+    stub_df_mount(expanded_home_code_dir, "/System/Volumes/Data")
+    @fake_system.mkdir_p(expanded_home_code_dir)
 
     step.run
 
-    assert_executed("sudo mdutil -i off #{expanded_home_code_dir}", quiet: false)
+    assert @fake_system.file_exist?(File.join(expanded_home_code_dir, ".metadata_never_index"))
   end
 
   def test_complete_when_battery_mode_installed
@@ -62,6 +64,7 @@ class ConfigureSpotlightIndexingStepTest < StepTestCase
 
   def test_incomplete_when_indexing_enabled_on_disabled_volume
     write_spotlight_config("battery_disable" => false, "disabled_volumes" => ["/Volumes/Archive"])
+    stub_df_mount("/Volumes/Archive", "/Volumes/Archive")
     stub_mdutil_status("/Volumes/Archive", "Indexing enabled.")
 
     assert_incomplete
@@ -69,9 +72,18 @@ class ConfigureSpotlightIndexingStepTest < StepTestCase
 
   def test_complete_when_indexing_disabled_on_disabled_volume
     write_spotlight_config("battery_disable" => false, "disabled_volumes" => ["/Volumes/Archive"])
+    stub_df_mount("/Volumes/Archive", "/Volumes/Archive")
     stub_mdutil_status("/Volumes/Archive", "Indexing disabled.")
 
     assert_complete
+  end
+
+  def test_incomplete_when_metadata_never_index_missing
+    write_spotlight_config("battery_disable" => false, "disabled_volumes" => ["~/Documents/Code.nosync"])
+    stub_df_mount(expanded_home_code_dir, "/System/Volumes/Data")
+    @fake_system.mkdir_p(expanded_home_code_dir)
+
+    assert_incomplete
   end
 
   private
@@ -95,6 +107,14 @@ class ConfigureSpotlightIndexingStepTest < StepTestCase
 
   def stub_mdutil_status(volume, status_line)
     @fake_system.stub_command("mdutil -s #{volume}", "#{volume}:\n\t#{status_line}", 0)
+  end
+
+  def stub_df_mount(path, mount_point)
+    output = <<~OUT
+      Filesystem 512-blocks Used Available Capacity iused ifree %iused Mounted on
+      /dev/disk3s1s1 3896910480 1 1 1% 1 1 1% #{mount_point}
+    OUT
+    @fake_system.stub_command("df -P #{path}", output, 0)
   end
 
   def fish_path
