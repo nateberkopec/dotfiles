a67b55f6b2750de5c411263af5c3515fefdc3a3a
Nate Berkopec <nate.berkopec@gmail.com>
2026-01-26 07:26:30 +0900

    Exclude non-root paths from Spotlight via metadata file

--- lib/dotfiles/steps/configure_spotlight_indexing_step.rb
+++ lib/dotfiles/steps/configure_spotlight_indexing_step.rb
@@ -23,7 +23,15 @@ class Dotfiles::Step::ConfigureSpotlightIndexingStep < Dotfiles::Step
     end
 
     disabled_volumes.each do |volume|
-      add_error("Spotlight indexing still enabled for #{volume}") unless indexing_disabled?(volume)
+      if volume_root?(volume)
+        add_error("Spotlight indexing still enabled for #{volume}") unless indexing_disabled?(volume)
+      else
+        unless @system.dir_exist?(volume)
+          add_error("Spotlight exclusion directory missing: #{volume}")
+          next
+        end
+        add_error("Spotlight exclusion file missing for #{volume}") unless metadata_never_index_exists?(volume)
+      end
     end
 
     @errors.empty?
@@ -61,9 +69,12 @@ class Dotfiles::Step::ConfigureSpotlightIndexingStep < Dotfiles::Step
 
   def disable_configured_volumes
     disabled_volumes.each do |volume|
-      next if indexing_disabled?(volume)
-
-      execute("mdutil -i off #{shell_escape(volume)}", sudo: true)
+      if volume_root?(volume)
+        next if indexing_disabled?(volume)
+        execute("mdutil -i off #{shell_escape(volume)}", sudo: true)
+      else
+        ensure_metadata_never_index(volume)
+      end
     end
   end
 
@@ -71,6 +82,21 @@ class Dotfiles::Step::ConfigureSpotlightIndexingStep < Dotfiles::Step
     output, status = execute("mdutil -s #{shell_escape(volume)}", quiet: true)
     return false unless status == 0
     output.downcase.include?("disabled")
+  end
+
+  def ensure_metadata_never_index(path)
+    return if metadata_never_index_exists?(path)
+    return unless @system.dir_exist?(path)
+
+    @system.write_file(metadata_never_index_path(path), "")
+  end
+
+  def metadata_never_index_exists?(path)
+    @system.file_exist?(metadata_never_index_path(path))
+  end
+
+  def metadata_never_index_path(path)
+    File.join(path, ".metadata_never_index")
   end
 
   def script_content
@@ -209,6 +235,18 @@ class Dotfiles::Step::ConfigureSpotlightIndexingStep < Dotfiles::Step
 
   def normalize_volumes(volumes)
     Array(volumes).compact.map { |volume| expand_path_with_home(volume) }.uniq
+  end
+
+  def volume_root?(path)
+    mount_point_for(path) == path
+  end
+
+  def mount_point_for(path)
+    output, status = execute("df -P #{shell_escape(path)}", quiet: true)
+    return nil unless status == 0
+    lines = output.split("\n")
+    return nil if lines.length < 2
+    lines.last.split(/\s+/).last
   end
 
   def shell_escape(value)
