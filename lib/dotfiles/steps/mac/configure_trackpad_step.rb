1fb6ee5e4d3b9ec64e85e18ad90794509e7245e5
Nate Berkopec <nate.berkopec@gmail.com>
2026-02-07 18:04:24 -0700

    Replace DefaultsStep with mixin

--- lib/dotfiles/steps/mac/configure_trackpad_step.rb
+++ lib/dotfiles/steps/mac/configure_trackpad_step.rb
@@ -1,4 +1,6 @@
-class Dotfiles::Step::ConfigureTrackpadStep < Dotfiles::Step::DefaultsStep
+class Dotfiles::Step::ConfigureTrackpadStep < Dotfiles::Step
+  include Dotfiles::Step::DefaultsConfigurable
+
   defaults_config_key "trackpad_settings"
   defaults_display_name "Trackpad"
 end
