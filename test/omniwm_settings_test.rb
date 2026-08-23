0e979b01b815b2ce91050da9bfb64875700f74c4
Nate Berkopec <nate.berkopec@gmail.com>
2026-08-24 06:02:48 +0900

    Configure OmniWM widths per monitor

--- test/omniwm_settings_test.rb
+++ test/omniwm_settings_test.rb
@@ -8,11 +8,27 @@ class OmniWMSettingsTest < Minitest::Test
     assert_equal "Unassigned", hotkeys.fetch("setContainerPrimarySpan.increase10Percent")
   end
 
+  def test_uses_monitor_specific_automatic_column_widths
+    refute settings.fetch("niri").key?("defaultContainerPrimarySpan")
+    assert_equal 3, visible_container_counts.fetch("DELL G3223Q")
+    assert_equal 2, visible_container_counts.fetch("Built-in Retina Display")
+  end
+
   private
 
+  def settings
+    @settings ||= TomlRB.load_file(settings_path)
+  end
+
   def hotkeys
-    @hotkeys ||= TomlRB.load_file(settings_path).fetch("hotkeys").to_h do |hotkey|
+    @hotkeys ||= settings.fetch("hotkeys").to_h do |hotkey|
       [hotkey.fetch("id"), hotkey.fetch("binding")]
+    end
+  end
+
+  def visible_container_counts
+    settings.fetch("monitorNiriOverrides").to_h do |monitor|
+      [monitor.fetch("monitorName"), monitor.fetch("visibleContainerCount")]
     end
   end
 
